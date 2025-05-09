e0bcf7d32696da952c0301a19ce13716588d987c
SivaAkm <gsivaakm2001@gmail.com>
2025-05-09 13:31:49 +0530

    url Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import './css/App.css'
 import NavBar from './components/NavBar'
 import Favorites from './Pages/Favorites'
 import HomePage from './Pages/HomePage'
-import {Routes,Route} from 'react-router-dom'
+import {Routes,Route,useNavigate} from 'react-router-dom'
 import { MovieProvider } from './contexts/MovieContext'
+
+
 function App() {
+  
   return (
     <MovieProvider>
+      
     <NavBar/>
-     
+   
     <Routes>
-      <Route path = {process.env.PUBLIC_URL + "/"} element={<HomePage/>}/>
-      <Route path = {process.env.PUBLIC_URL +"/Home"} element={<HomePage/>}/>
-      <Route path = {process.env.PUBLIC_URL +"/MyFavorites"} element={<Favorites/>}/>
+      <Route path = "/MovieApllication" element={<HomePage/>}/>
+      <Route path = "/Home" element={<HomePage/>}/>
+      <Route path = "/MyFavorites"  element={<Favorites/>}/>
     </Routes>
       
       </MovieProvider>
